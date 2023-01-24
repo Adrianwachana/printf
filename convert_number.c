b01ee642cf269a5110e5d13ec8255ce22f804a2d
Kabiruux <uxkabiru@gmail.com>
2023-01-24 00:09:07 -0800

    changed file

--- convert_number.c
+++ convert_number.c
@@ -66,6 +66,25 @@ int print_HEX(va_list ap, params_t *params)
  *
  * Return: bytes printed
  */
+int print_binary(va_list ap, params_t *params)
+{
+	unsigned int n = va_arg(ap, unsigned int);
+	char *str = convert(n, 2, CONVERT_UNSIGNED, params);
+	int c = 0;
+
+	if (params->hashtag_flag && n)
+		*--str = '0';
+	params->unsign = 1;
+	return (c += print_number(str, params));
+}
+
+/**
+ * print_octal - prints unsigned octal numbers
+ * @ap: the argument pointer
+ * @params: the parameters struct
+ *
+ * Return: bytes printed
+ */
 int print_octal(va_list ap, params_t *params)
 {
 	unsigned long l;
